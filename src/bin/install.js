499bdb7779bed61a4c4f4b80d3f4d938426f2410
shinout <shinout310@gmail.com>
2016-02-14 14:13:04 +0900

    add a script for installation #2

---
+++ src/bin/install.js
@@ -0,0 +1,34 @@
+require('shelljs/global')
+import {resolve} from 'path'
+
+const log = ::console.log
+
+function run() {
+    log('begin installing faster-titanium')
+    const tiPath = which('titanium')
+    if (!tiPath) {
+        log('titanium command not found. Finish installation.')
+        return
+    }
+
+    const hookPath = resolve(__dirname, '../../dist/hook/faster.js')
+    const ticonf = JSON.parse(exec(`${tiPath} config -o json`, {silent: true}).output)
+
+    if (ticonf['paths.hooks'] && ticonf['paths.hooks'].indexOf(hookPath) >= 0) {
+        log('already installed')
+        return
+    }
+
+    const result = exec(`${tiPath} -q config paths.hooks -a ${hookPath}`)
+
+
+    if (result.code) {
+        log('installation failed')
+        log(result)
+    }
+    else {
+        log('installation succeeded')
+    }
+}
+
+if (require.main === module) run()
