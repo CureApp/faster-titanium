eb375d966150d031f285e9ed3af2b166a43dad58
CircleCI <circleci@cureapp.jp>
2016-02-26 10:31:29 +0000

    v1.6.1

---
+++ dist/server/content-responder.js
@@ -0,0 +1,179 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _path = require('path');
+
+var _fs = require('fs');
+
+var _browserify = require('browserify');
+
+var _browserify2 = _interopRequireDefault(_browserify);
+
+var _responseInfo = require('./response-info');
+
+var _responseInfo2 = _interopRequireDefault(_responseInfo);
+
+var _resourceLoader = require('./resource-loader');
+
+var _resourceLoader2 = _interopRequireDefault(_resourceLoader);
+
+var _appJsConverter = require('./app-js-converter');
+
+var _appJsConverter2 = _interopRequireDefault(_appJsConverter);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/**
+ * return Promise of ResponseInfo by URL
+ */
+
+var ContentResponder = function () {
+    function ContentResponder() {
+        _classCallCheck(this, ContentResponder);
+    }
+
+    _createClass(ContentResponder, [{
+        key: 'respond',
+
+
+        /**
+         * create ResponseInfo
+         * @param {string|Buffer} content
+         * @param {string} [contentType=text/plain]
+         * @param {number} [statusCode=200]
+         * @return {Promise<ResponseInfo>}
+         */
+        value: function respond(content, contentType, statusCode) {
+            return Promise.resolve(new _responseInfo2.default(content, contentType, statusCode));
+        }
+
+        /**
+         * create ResponseInfo of JSON
+         * @param {Object} obj
+         * @param {string} [contentType=application/json]
+         * @param {number} [statusCode=200]
+         * @return {Promise<ResponseInfo>}
+         */
+
+    }, {
+        key: 'respondJSON',
+        value: function respondJSON(obj) {
+            var contentType = arguments.length <= 1 || arguments[1] === undefined ? 'application/json' : arguments[1];
+            var statusCode = arguments[2];
+
+            return this.respond(JSON.stringify(obj), contentType, statusCode);
+        }
+
+        /**
+         * create ResponseInfo of file
+         * @param {string} path
+         * @param {string} [contentType=text/plain]
+         * @param {number} [statusCode=200]
+         * @return {Promise<ResponseInfo>}
+         */
+
+    }, {
+        key: 'respondFile',
+        value: function respondFile(path, contentType, statusCode) {
+            var _this = this;
+
+            return new Promise(function (y, n) {
+                (0, _fs.readFile)(path, 'utf8', function (e, o) {
+                    return e ? n(e) : y(o);
+                });
+            }).then(function (content) {
+                return _this.respond(content, contentType, statusCode);
+            });
+        }
+
+        /**
+         * create ResponseInfo of NOT FOUND
+         * @param {string} url
+         * @return {Promise<ResponseInfo>}
+         */
+
+    }, {
+        key: 'notFound',
+        value: function notFound(url) {
+            return this.respond('404 not found: ' + url, 'text/plain', 404);
+        }
+
+        /**
+         * load web/index.html
+         * @return {Promise<ResponseInfo>}
+         */
+
+    }, {
+        key: 'webUI',
+        value: function webUI() {
+            return this.respondFile(__dirname + '/../../web/index.html', 'text/html');
+        }
+
+        /**
+         * load dist/web/main.js after bundling by browserify
+         * @return {Promise<ResponseInfo>}
+         * @todo cache the result
+         */
+
+    }, {
+        key: 'webJS',
+        value: function webJS() {
+            var _this2 = this;
+
+            var mainJSPath = (0, _path.resolve)(__dirname, '../../dist/web/main.js');
+            return this.bundle(mainJSPath).then(function (buf) {
+                return _this2.respond(buf, 'text/javascript');
+            });
+        }
+    }, {
+        key: 'bundle',
+        value: function bundle(file, options) {
+            return new Promise(function (y, n) {
+                (0, _browserify2.default)(file, options).bundle(function (e, o) {
+                    return e ? n(e) : y(o);
+                });
+            });
+        }
+
+        /**
+         * load a file in Resources directory
+         * @param {string} url
+         * @param {string} projDir
+         * @param {string} platform
+         * @return {Promise<ResponseInfo>}
+         */
+
+    }, {
+        key: 'resource',
+        value: function resource(url, projDir, platform) {
+
+            // @type Buffer
+            var content = new _resourceLoader2.default(url, projDir, platform).content;
+            if (content === null) {
+                return this.notFound(url);
+            }
+
+            /**
+             * In app.js, top variables are exported as global variables.
+             * Thus, AppJsConverter converts the code as such.
+             * TODO Caches the result.
+             */
+            if (url === '/app.js') {
+                content = new _appJsConverter2.default(content.toString()).convert();
+            }
+
+            return this.respond(content);
+        }
+    }]);
+
+    return ContentResponder;
+}();
+
+exports.default = ContentResponder;
---
+++ dist/titanium/require-agent.js
@@ -0,0 +1,227 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+exports.relativePath = relativePath;
+
+var _module = require('./module');
+
+var _module2 = _interopRequireDefault(_module);
+
+var _http = require('./http');
+
+var _http2 = _interopRequireDefault(_http);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var ____ = function ____(v) {
+    var type = arguments.length <= 1 || arguments[1] === undefined ? 'log' : arguments[1];
+    return console[type]('[FasterTitanium:RequireAgent]', v);
+};
+
+var RequireAgent = function () {
+
+    /**
+     * @param {function} original (titanium) require function
+     * @param {string} host
+     * @param {number} port
+     */
+
+    function RequireAgent(origRequire, host, port) {
+        _classCallCheck(this, RequireAgent);
+
+        /** @type {function(moduleName: string):Object} */
+        this.origRequire = origRequire;
+
+        /** @type {Map<string, Module>} */
+        this.modules = {};
+
+        /** @type {number} */
+        this.timeout = 10000;
+
+        /** @type {number} */
+        this.host = host;
+
+        /** @type {number} */
+        this.host = host;
+
+        /** @type {number} */
+        this.port = parseInt(port, 10);
+    }
+
+    /**
+     * @param {string} moduleName
+     * @returns {object}
+     */
+
+
+    _createClass(RequireAgent, [{
+        key: 'require',
+        value: function require(moduleName) {
+
+            ____('requiring ' + moduleName);
+            if (this.modules[moduleName]) {
+                return this.modules[moduleName].exports;
+            }
+
+            var source = undefined;
+            try {
+                source = this.getServerSource(moduleName);
+            } catch (e) {
+                console.warn('Couldn\'t fetch module ' + moduleName + ' from HTTP server. Use local file.');
+                try {
+                    source = this.getLocalSource(moduleName);
+                } catch (e) {
+                    console.warn('Couldn\'t fetch module ' + moduleName + ' from file system. Use original require.');
+                    return this.origRequire(moduleName);
+                }
+            }
+            var mod = this.createModule(moduleName, source);
+            this.modules[moduleName] = mod;
+            return mod.exports;
+        }
+
+        /**
+         * require module after resolving relative path, stripping extension
+         * @param {string} rawModuleName
+         * @param {Module} moduleFrom module where this method is called
+         * @returns {object}
+         */
+
+    }, {
+        key: 'requireRaw',
+        value: function requireRaw(rawModuleName, moduleFrom) {
+
+            var moduleName = rawModuleName;
+
+            // resolve relative path
+            if (moduleName.match(/^\./)) {
+                moduleName = relativePath(moduleFrom.moduleName, moduleName);
+            }
+
+            if (moduleName.match(/\.js$/)) {
+                moduleName = moduleName.slice(0, -3);
+            }
+            return this.require(moduleName);
+        }
+
+        /**
+         * @param {string} moduleName
+         * @param {string} source
+         */
+
+    }, {
+        key: 'createModule',
+        value: function createModule(moduleName, source) {
+            var _this = this;
+
+            var mod = new _module2.default(moduleName);
+
+            var fn = Function('exports, require, module, __dirname, __filename', source);
+            fn(mod.exports, function (v) {
+                return _this.requireRaw(v, mod);
+            }, mod, mod.__dirname, mod.__filename);
+
+            return mod;
+        }
+
+        /**
+         * @param {string} moduleName
+         * @returns {string} js code
+         */
+
+    }, {
+        key: 'getServerSource',
+        value: function getServerSource(moduleName) {
+
+            var url = 'http://' + this.host + ':' + this.port + '/' + moduleName + '.js';
+            ____('\tremote access: ' + url);
+
+            return _http2.default.get(url, {
+                timeout: this.timeout
+            });
+        }
+
+        /**
+         * @param {string} moduleName
+         * @returns {string} js code
+         */
+
+    }, {
+        key: 'getLocalSource',
+        value: function getLocalSource(moduleName) {
+
+            ____('\tfile access: ' + moduleName);
+
+            if (moduleName === 'app') moduleName = 'original-app';
+
+            var file = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, moduleName + '.js');
+
+            return file.read().text;
+        }
+
+        /**
+         * clear cache by name
+         */
+
+    }, {
+        key: 'clearCache',
+        value: function clearCache(name) {
+            delete this.modules[name];
+        }
+
+        /**
+         * clear all module caches
+         */
+
+    }, {
+        key: 'clearAllCaches',
+        value: function clearAllCaches() {
+            for (key in this.modules) {
+                this.clearCache(key);
+            }
+        }
+    }]);
+
+    return RequireAgent;
+}();
+
+/**
+ * @param {string} from
+ * @param {string} to
+ * @private (export for test)
+ */
+
+
+exports.default = RequireAgent;
+function relativePath(from, to) {
+
+    var fromNodes = from.split('/');
+    fromNodes.pop();
+
+    var toNodes = to.split('/');
+
+    for (var i in toNodes) {
+        var toNode = toNodes[i];
+        switch (toNode) {
+            case '..':
+                if (fromNodes.length === 0) {
+                    throw new Error('cannot resolve relative path. from: ' + from + ', to: ' + to);
+                }
+                fromNodes.pop();
+                break;
+            case '.':
+                break;
+            default:
+                fromNodes.push(toNode);
+                break;
+        }
+    }
+    return fromNodes.join('/');
+}
